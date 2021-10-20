46c5536e4a2f6ac37807349383a21ad4adccc788
Oliver Norin <oliver@notima.se>
2021-10-20 12:17:11 +0200

    init

---
+++ include/modbus.h
@@ -0,0 +1,66 @@
+#include "stdint.h"
+
+/**
+ * Protocol Data Unit
+ * This structure contains the data of the 
+ * modbus request.
+ */
+struct modbus_pdu {
+    uint8_t functionCode;
+    uint16_t addrBegin;
+    uint16_t numOfRegisters;
+};
+
+/**
+ * Modbus application header for modbus tcp
+ * This structure conatains data that is 
+ * specific to modbus tcp requests.
+ */
+struct modbus_tcp_mbap {
+    uint16_t transactionId;
+    uint16_t protocolId;
+    uint16_t messageLength;
+    uint8_t unitId;
+};
+
+/**
+ * A complete request structure for modbus tcp
+ */
+struct modbus_tcp_request {
+    struct modbus_tcp_mbap mbap;
+    struct modbus_pdu pdu;
+};
+
+/**
+ * A complete request structure for modbus rtu
+ */
+struct modbus_rtu_request {
+    uint8_t slaveAddress;
+    struct modbus_pdu pdu;
+};
+
+/**
+ * Get the raw bytes of a modbus tcp request
+ * @param requestBuffer Where to write the bytes.
+ * Make sure enough memory is allocated at this 
+ * address!
+ * @param request The request
+ */
+void modbus_get_raw_tcp_request(char* requestBuffer, struct modbus_tcp_request request);
+
+/**
+ * Get the raw bytes of a modbus rtu request
+ * @param requestBuffer Where to write the bytes.
+ * Make sure enough memory is allocated at this 
+ * address!
+ * @param request The request
+ */
+void modbus_get_raw_rtu_request(char* requestBuffer, struct modbus_rtu_request request);
+
+struct modbus_pdu modbus_read_discrete_output(uint16_t addrBegin, uint16_t numOfRegisters);
+
+struct modbus_pdu modbus_read_discrete_input(uint16_t addrBegin, uint16_t numOfRegisters);
+
+struct modbus_pdu modbus_read_analog_output(uint16_t addrBegin, uint16_t numOfRegisters);
+
+struct modbus_pdu modbus_read_analog_input(uint16_t addrBegin, uint16_t numOfRegisters);
---
+++ src/modbus.c
@@ -0,0 +1,64 @@
+#include "modbus.h"
+#include <stdbool.h>
+
+uint16_t mod_rtu_crc(char buf[], int len)
+{
+  uint16_t crc = 0xFFFF;
+  
+  for (int pos = 0; pos < len; pos++) {
+    crc ^= (uint16_t)buf[pos];
+  
+    for (int i = 8; i != 0; i--) {
+      if ((crc & 0x0001) != 0) {
+        crc >>= 1;
+        crc ^= 0xA001;
+      }
+      else
+        crc >>= 1;
+    }
+  }
+  // Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+  return crc;  
+}
+
+void modbus_get_raw_tcp_request(char* requestBuffer, struct modbus_tcp_request request) {
+    requestBuffer[0] = request.mbap.transactionId >> 8;
+    requestBuffer[1] = request.mbap.transactionId;
+    requestBuffer[2] = request.mbap.protocolId >> 8;
+    requestBuffer[3] = request.mbap.protocolId;
+    requestBuffer[4] = request.mbap.messageLength >> 8;
+    requestBuffer[5] = request.mbap.messageLength;
+    requestBuffer[6] = request.mbap.unitId;
+
+    requestBuffer[7] = request.pdu.functionCode;
+    requestBuffer[8] = request.pdu.addrBegin >> 8;
+    requestBuffer[9] = request.pdu.addrBegin;
+    requestBuffer[10] = request.pdu.numOfRegisters >> 8;
+    requestBuffer[11] = request.pdu.numOfRegisters;
+}
+
+void modbus_get_raw_rtu_request(char* requestBuffer, struct modbus_rtu_request request) {
+    requestBuffer[0] = request.slaveAddress;
+    requestBuffer[1] = request.pdu.functionCode;
+    requestBuffer[2] = request.pdu.addrBegin >> 8;
+    requestBuffer[3] = request.pdu.addrBegin;
+    requestBuffer[4] = request.pdu.numOfRegisters >> 8;
+    requestBuffer[5] = request.pdu.numOfRegisters;
+
+    uint16_t crc = mod_rtu_crc(requestBuffer, 6);
+
+    requestBuffer[6] = crc;
+    requestBuffer[7] = crc >> 8;
+}
+
+struct modbus_pdu modbus_read_analog_input(uint16_t addrBegin, uint16_t numOfRegisters) {
+    struct modbus_pdu pdu;
+    pdu.functionCode = 4;
+    pdu.addrBegin = addrBegin;
+    pdu.numOfRegisters = numOfRegisters;
+    return pdu;
+}
+
+bool mgos_modbus_init(void) {
+    return true;
+}
